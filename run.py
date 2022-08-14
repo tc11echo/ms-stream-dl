0999c545b2cbf8a1c92eeb2166b0fbcfe8b55861
Thomas Chan <69757520+tc11echo@users.noreply.github.com>
2022-08-14 23:55:18 +0800

    Test

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
+import bs4
+import os
+
 with open(r"source.txt", "r", encoding="utf-8") as fpr:
     data=fpr.read()
 
-import bs4
 root=bs4.BeautifulSoup(data, "html.parser")
 links=root.find_all("a", class_="c-hyperlink c-caption-1 video-list-item-title no-hover")
 
 count=0
-
 with open(r"video_links.txt", "w", encoding="utf-8") as fpw:
     for i in links:
         fpw.write("https://web.microsoftstream.com"+i["href"]+"\n")
@@ -16,7 +17,6 @@ print(f"done, in total {count} video(s)")
 print("================================")
 print("start downloading")
 
-import os
 path=os.path.join(os. getcwd(), "video")
 if(not os.path.exists("video")):
     os.mkdir(path)
